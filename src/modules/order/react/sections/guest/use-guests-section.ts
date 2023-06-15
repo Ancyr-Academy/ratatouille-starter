22df35212eb0b6970c8b849e27edc28071a7f7a3
Anthony Cyrille <ancyr@pm.me>
2023-06-15 08:29:03 +0200

    task: guests state rehydration

--- src/modules/order/react/sections/guest/use-guests-section.ts
+++ src/modules/order/react/sections/guest/use-guests-section.ts
@@ -2,8 +2,9 @@ import { useRef, useState } from "react";
 import { useDependencies } from "@ratatouille/modules/app/react/DependenciesProvider";
 import { GuestForm } from "@ratatouille/modules/order/core/form/guest.form";
 import { OrderingDomainModel } from "@ratatouille/modules/order/core/model/ordering.domain-model";
-import { useAppDispatch } from "@ratatouille/modules/store/store";
+import { AppState, useAppDispatch } from "@ratatouille/modules/store/store";
 import { chooseGuests } from "@ratatouille/modules/order/core/usecases/choose-guests.usecase";
+import { useSelector } from "react-redux";
 
 export const useGuestsSection = () => {
   function addGuest() {
@@ -38,13 +39,12 @@ export const useGuestsSection = () => {
     return guestForm.current.isSubmittable(form);
   }
 
+  const initialForm = useSelector((state: AppState) => state.ordering.form);
+
   const dispatch = useAppDispatch();
   const { idProvider } = useDependencies();
   const guestForm = useRef(new GuestForm(idProvider));
-  const [form, setForm] = useState<OrderingDomainModel.Form>({
-    guests: [],
-    organizerId: null,
-  });
+  const [form, setForm] = useState<OrderingDomainModel.Form>(initialForm);
 
   return {
     addGuest,
